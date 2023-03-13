020cabec6499a7f40f53e2fc24cf9fbba84e1068
daftboston <dsantoyop@gmail.com>
2023-03-13 09:09:55 -0500

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Component, useState } from 'react'
 
 import './App.css'
+import Form from './components/Form'
+import ToDoForm from "./components/ToDoForm"
 
 
 
 
 function App() {
+
 
  
 
@@ -15,6 +18,12 @@ function App() {
 
   return (
     <div className="App">
+      <h1>REACT HOOK FORM</h1>
+
+      <Form></Form>
+      <h2>to do form</h2>
+      <ToDoForm></ToDoForm>
+
         
     
     </div>
